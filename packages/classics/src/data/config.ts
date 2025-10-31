9145ad6ca85420baafcf421f71356f865995d270
Anamol <anamol.maharjan.dev@gmail.com>
2025-10-31 20:45:02 +0545

    Fixed Endpoint

--- packages/classics/src/data/config.ts
+++ packages/classics/src/data/config.ts
@@ -1,2 +1,2 @@
-//export const endpoint = "https://api.classicsprojects.com.np";
-export const endpoint = "http://192.168.101.3:8000";
+export const endpoint = "https://api.classicsprojects.com.np";
+//export const endpoint = "http://192.168.101.3:8000";
